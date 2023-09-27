868619dec4f4f0740369cb017cf603d2895c1146
Arkadiy <goryunov.arkadiy@gmail.com>
2023-09-27 15:01:08 +0700

    Fix circle

--- src/calculator/figures/circle.py
+++ src/calculator/figures/circle.py
@@ -1,13 +1,15 @@
 import math
 
 from src.calculator.figures.figures_parent import FigureParent
+from src.calculator.utils.validator import Validator
 
 
 class Circle(FigureParent):
     @staticmethod
     def calculate_area(radius):
         if len(radius) == 1 or type(radius) == str:
-            radius = Circle.assert_values(radius).pop()
+            radius = Validator.assert_input_value(radius)
             area = math.pi * radius * radius
+            print(area)
             return round(area, 2)
         raise Exception("You enter to many arguments!")
